35b5e8369432425e200576c787d34f6a415f92d3
m-wellerson <m_wellerson@outlook.com>
2020-10-27 09:42:15 -0300

    Added Cors

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');        // call express
 var bodyParser = require('body-parser');
 var winston = require('./config/winston');
+var cors = require('cors');
 var Config = require('./config/config'),
     config = new Config();
 
@@ -29,6 +30,15 @@ server.use(function (err, req, res, next) {
     if (err.name === 'UnauthorizedError') {
         res.status(401).json({message: 'Missing or invalid token'});
     }
+});
+
+server.use((req, res, next) => {
+	//Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
+    res.header("Access-Control-Allow-Origin", "*");
+	//Quais são os métodos que a conexão pode realizar na API
+    res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
+    server.use(cors());
+    next();
 });
 
 
